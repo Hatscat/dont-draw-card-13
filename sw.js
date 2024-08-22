0b26a5a95de82fe48b720624502c9426d06b4f0e
Hatscat <luboudy@gmail.com>
2024-08-22 21:37:34 +0000

    Deploying to gh-pages from @ Hatscat/dont-draw-card-13@5bd22c435af438c42757ab27bedf8512101db1fd 🚀

--- sw.js
+++ sw.js
@@ -1,13 +1,9 @@
-const cacheName = "template-store-v1";
-
-// Installing Service Worker
 self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(cacheName).then((cache) => cache.addAll(["/index.html"]))
+    caches.open("card13-v1").then((cache) => cache.addAll(["/index.html"]))
   );
 });
 
-// Fetching content using Service Worker
 self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request))
